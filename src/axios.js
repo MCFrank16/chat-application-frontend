d38e73b0c9cd30d1ac8e25b5367935b7c267c6af
MCFrank16 <=>
2021-03-02 10:31:31 +0200

    configuring axios

---
+++ src/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const request = axios.create({
+    baseURL: `https://cata-16.herokuapp.com`
+});
+
+export default request;
--- src/components/Login-container.js
+++ src/components/Login-container.js
@@ -1,26 +1,12 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useToken from './useToken';
+import axios from '../axios';
+
 
 const LoginUser = async (credentials) => {
-
-	console.log(process.env.PROD_URL);
-
-	const result = await fetch(`https://realtime-chat-ossix.herokuapp.com/create/user`, {
-		method: 'POST',
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(credentials)
-	});
-
-	console.log(result);
-
-	// return {
-	// 	status: 200,
-	// 	token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiTUNGcmFuazE2IiwiaWF0IjoxNjE0NjAyMzUyLCJleHAiOjE2MTQ2ODg3NTJ9.3fFb7ZUC_IJwgJth-lK3J3V4ToNSvVEX3wC_54_plLI"
-	// };
+	const result = await axios.post('/login/user', JSON.stringify(credentials));
+	return result.data;
 }
 
 const LoginContainer = () => {
